40405f6b9c01f49031d4f6e592d67722edcd9e42
dimassatrio18 <if20.dimassatrioekasaputro@mhs.ubpkarawang.ac.id>
2022-08-09 16:42:50 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// panggil ProductController sebagai object
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+
+Route::middleware(['jwt-auth'])->group(function() {
+    // route untuk menambahkan produk data produk
+    Route::post('/product',[ProductController::class,'store']);
+
+    // route untuk menampilkan data produk keseluruhan
+    Route::get('/product',[ProductController::class,'showAll']);
+
+    // route untuk menampilkan data produk berdasarkan ID
+    Route::get('/product/{id}',[ProductController::class,'showById']);
+
+    // route untuk menampilkan data produk berdasarkan nama
+    Route::get('/product/search/product_name={product_name}',[ProductController::class,'showByName']);
+
+    // route untuk mengubah data produk berdasarkan ID produk
+    Route::put('/product/{id}',[ProductController::class,'update']);
+
+    // route untuk menghapus data produk berdasarkan ID produk
+    Route::delete('/product/{id}',[ProductController::class,'delete']);
+});
+// route untuk login
+Route::post('/login', [UserController::class,'login']);
